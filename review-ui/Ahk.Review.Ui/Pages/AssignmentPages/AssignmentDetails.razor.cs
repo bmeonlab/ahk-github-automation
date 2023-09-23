567c5bc6cd3a964abd992bfca39662c902930c35
balintgrober <groberbalint@gmail.com>
2023-09-23 18:13:02 +0200

    Assignment details

--- review-ui/Ahk.Review.Ui/Pages/AssignmentPages/AssignmentDetails.razor.cs
+++ review-ui/Ahk.Review.Ui/Pages/AssignmentPages/AssignmentDetails.razor.cs
@@ -1,8 +1,31 @@
+using Ahk.Review.Ui.Models;
+using Ahk.Review.Ui.Services;
+
 using Microsoft.AspNetCore.Components;
 
 namespace Ahk.Review.Ui.Pages.AssignmentPages
 {
     public partial class AssignmentDetails : ComponentBase
     {
+        [Parameter]
+        public string AssignmentId { get; set; }
+
+        [Inject]
+        private AssignmentService AssignmentService { get; set; }
+        [Inject]
+        private SubjectService SubjectService { get; set; }
+
+        private Assignment assignment;
+        private string subjectName;
+        private List<Exercise> exerciseList;
+
+        protected override async void OnInitialized()
+        {
+            assignment = await AssignmentService.GetAssignmentAsync(SubjectService.CurrentTenant.SubjectCode, AssignmentId);
+            subjectName = SubjectService.CurrentTenant.Name;
+            exerciseList = await AssignmentService.GetExercisesAsync(SubjectService.CurrentTenant.SubjectCode, AssignmentId);
+
+            StateHasChanged();
+        }
     }
 }
